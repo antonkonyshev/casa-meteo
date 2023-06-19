f455531f4a7b84db65f6ec6869a22b472a428581
Anton Konyshev <anton.konyshev@gmail.com>
2023-06-19 11:01:46 +0300

    Measurements history API endpoint. Response measurements JSON generation has been partially refactored.

--- src/main.cpp
+++ src/main.cpp
@@ -22,35 +22,31 @@
 // significantly in case more details are needed.
 #define MEASUREMENT_PERIOD 60  // seconds; in order to have last 24 hours, use 5m period and history length of 288, it'll require 
 #define HISTORY_LENGTH 60  // measurements
+#define SERIALIZED_MEASUREMENT_MAX_LENGTH 256  // near 200 length de facto
 
 static const char* wifi_ssid = "AKTpLAp";
 static const char* wifi_password = "JXfRV9QRD7EaFW6UAPNPoG2BdXB2LcpETSsJmpmX7R9gr5Xn6qoyq2bpLKCn7A4";
 
 Adafruit_BMP280 bmp280;
 WebServer server(API_PORT);
-StaticJsonDocument<256> jsonDocument;  // ~200 length de facto
-char buffer[256];
 time_t now = 0;
 hw_timer_t* measurement_timer = NULL;
 bool perform_periodical_measurement = false;
-uint8_t history_index = 0;
+uint8_t history_index = -1;
 
 typedef struct measurement_s {
-  uint32_t timestamp;
-  float temperature;
-  float pressure;
-  float altitude;
+  uint32_t timestamp;  // UTC timezone
+  float temperature;  // °C
+  float pressure;  // mmHg
+  float altitude;  // meters above sea level
 } measurement_t;
 
 measurement_t* history[HISTORY_LENGTH];
 
-void loadTime() {
-  time(&now);
-}
-
 measurement_t* loadSensorData() {
   measurement_t* measurement;
   measurement = (measurement_t*) malloc(sizeof(measurement_t));
+  time(&now);
   measurement->timestamp = now;
   Serial.print("Timestamp: ");
   Serial.print(measurement->timestamp);
@@ -77,39 +73,69 @@ measurement_t* loadSensorData() {
   return measurement;
 }
 
-void createJson(measurement_t* measurement) {
-  jsonDocument.clear();
-  jsonDocument["timestamp"] = measurement->timestamp;
-  JsonObject obj = jsonDocument.createNestedObject("temperature");
+void createJson(measurement_t* measurement, JsonObject* json_object) {
+  json_object->operator[]("timestamp") = measurement->timestamp;
+  JsonObject obj = json_object->createNestedObject("temperature");
   obj["value"] = measurement->temperature;
   obj["unit"] = "°C";
-  obj = jsonDocument.createNestedObject("pressure");
+  obj = json_object->createNestedObject("pressure");
   obj["value"] = measurement->pressure;
   obj["unit"] = "mmHg";
-  obj = jsonDocument.createNestedObject("altitude");
+  obj = json_object->createNestedObject("altitude");
   obj["value"] = measurement->altitude;
   obj["unit"] = "m";
-  serializeJson(jsonDocument, buffer);
 }
 
 void performPeriodicalMeasurement() {
   perform_periodical_measurement = true;
 }
 
+void getServiceIdResponse() {
+  server.send(200, "plain/text", "meteo");
+}
+
 void getMeteoResponse() {
   digitalWrite(LED_PIN, HIGH);
-  createJson(loadSensorData());
+  measurement_t* measurement = loadSensorData();
+  StaticJsonDocument<SERIALIZED_MEASUREMENT_MAX_LENGTH> json_document;
+  JsonObject json_object = json_document.to<JsonObject>();
+  createJson(measurement, &json_object);
+  char buffer[SERIALIZED_MEASUREMENT_MAX_LENGTH];
+  serializeJson(json_document, buffer);
   server.send(200, "application/json", buffer);
   digitalWrite(LED_PIN, LOW);
 }
 
-void getServiceID() {
-  server.send(200, "plain/text", "meteo");
+void getHistoryResponse() {
+  digitalWrite(LED_PIN, HIGH);
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  server.send(200, "application/json", "[");
+  char buffer[SERIALIZED_MEASUREMENT_MAX_LENGTH] = { '\0' };
+  DynamicJsonDocument json_document(SERIALIZED_MEASUREMENT_MAX_LENGTH * HISTORY_LENGTH);
+  for (uint8_t idx = 0; idx < HISTORY_LENGTH; idx++) {
+    if (history[idx] == NULL) {
+      break;
+    }
+    memset(&buffer[0], 0, sizeof(buffer));
+    StaticJsonDocument<SERIALIZED_MEASUREMENT_MAX_LENGTH> json_document;
+    JsonObject json_object = json_document.to<JsonObject>();
+    createJson(history[idx], &json_object);
+    if (idx > 0) {
+      server.sendContent(",");
+    }
+    serializeJson(json_document, buffer);
+    server.sendContent(buffer);
+  }
+  server.sendContent("]");
+  server.sendContent("");
+  server.client().stop();
+  digitalWrite(LED_PIN, LOW);
 }
 
 void setupRouting() {
   server.on("/", getMeteoResponse);
-  server.on("/service", getServiceID);
+  server.on("/service", getServiceIdResponse);
+  server.on("/history", getHistoryResponse);
   server.begin();
 }
 
@@ -123,6 +149,14 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
   Serial.println(" [ Done ]");
+
+  Serial.print("Disabling Bluetooth module ...");
+  if (btStop()) {
+    Serial.println(" [ Done ]");
+  } else {
+    Serial.print(" [ Fail ]");
+  }
+
   Serial.print("Initializing BMP280 ...");
   if (bmp280.begin(BMP280_I2C_ADDRESS)) {
     Serial.println(" [ Done ]");
@@ -139,17 +173,17 @@ void setup() {
   }
   Serial.println(" [ Done ]");
 
-  Serial.print("Starting HTTP API server ...");
-  setupRouting();
-  Serial.println(" [ Done ]");
-
   Serial.print("Setting up the real time clock ...");
   configTime(0, 0, "pool.ntp.org", "time.google.com", "time.cloudflare.com");
   while (now < 1687100000) {
-    loadTime();
+    time(&now);
     Serial.print(".");
     delay(100);
   }
+  Serial.println(" [ Done ]");
+
+  Serial.print("Starting HTTP API server ...");
+  setupRouting();
   Serial.println(" [ Done ]");
 
   Serial.print("Setting up timers ...");
