f4845c0696dcf09d629d9000a377e395a8cd867e
Anton Konyshev <anton.konyshev@gmail.com>
2024-05-06 18:59:00 +0300

    Common logic of the API endpoint requests handling moved to the CasaLib.

--- src/server.cpp
+++ src/server.cpp
@@ -2,6 +2,12 @@
 
 AsyncWebServer server(API_PORT);
 
+void serviceInfoResponsePayload(char* buffer) {
+    sprintf(buffer,
+        "{\"service\":\"meteo\",\"name\":\"Room\",\"id\":\"%s\",\"sensors\":[\"temperature\",\"pressure\",\"altitude\",\"pollution\"]}",
+        DEVICE_ID);
+}
+
 void setupRouting() {
     server.on("/", [](AsyncWebServerRequest* request) {
         digitalWrite(LED_PIN, HIGH);
@@ -9,49 +15,10 @@ void setupRouting() {
         digitalWrite(LED_PIN, LOW);
     });
 
-    server.on("/journal", [](AsyncWebServerRequest* request) {
-        digitalWrite(LED_PIN, HIGH);
-        journal_t* journal = getJournal();
-        if (!journal->first) {
-            request->send(200, "application/json", "[]");
-            return;
-        }
-        record_t* record = journal->first;
-        AsyncResponseStream* response = request->beginResponseStream("application/json");
-        response->print("[");
-        while (record) {
-            if (record != journal->first) {
-                response->print(",");
-            }
-            response->print(record->message);
-            record = record->next;
-        }
-        response->print("]");
-        request->send(response);
-        digitalWrite(LED_PIN, LOW);
-    });
-
-    server.on("/service", [](AsyncWebServerRequest* request) {
-        digitalWrite(LED_PIN, HIGH);
-        // The service endpoint response is a constant for the service, since it doesn't changes within time while the device is working
-        request->send(200, "application/json", "{\"service\":\"meteo\",\"name\":\"Room\",\"id\":\"" + String(DEVICE_ID) + "\",\"sensors\":[\"temperature\",\"pressure\",\"altitude\",\"pollution\"]}");
-        digitalWrite(LED_PIN, LOW);
-    });
-
-    server.on("/settings", HTTP_POST, [](AsyncWebServerRequest* request) {
-        digitalWrite(LED_PIN, HIGH);
-        saveSettings(request);
-        request->send(200);
-        digitalWrite(LED_PIN, LOW);
-    });
-
-    server.on("/settings", [](AsyncWebServerRequest* request) {
-        digitalWrite(LED_PIN, HIGH);
-        char payload[512] = {0};
-        serializeSettings(payload);
-        request->send(200, "application/json", (const char*) payload);
-        digitalWrite(LED_PIN, LOW);
-    });
+    server.on("/journal", handleJournalRequest);
+    server.on("/service", handleServiceInfoRequest);
+    server.on("/settings", HTTP_POST, handleSettingsEditRequest);
+    server.on("/settings", handleSettingsReadRequest);
 
     server.begin();
 }
--- src/server.h
+++ src/server.h
@@ -3,9 +3,10 @@
 #include <ESPAsyncWebServer.h>
 
 #include "pinout.h"
-#include "preferences.h"
+#include "CasaLib/server.h"
 #include "sensors.h"
 
 #define API_PORT 80
 
+void serviceInfoResponsePayload(char* buffer);
 void setupRouting();
